48928dac23c5d1b93581fd3bb7e95722242a8f11
Donald Ballowe <dballowe7912@gmail.com>
2021-02-20 12:51:22 -0600

    Added rolling animation @keyframes

--- src/Dice.js
+++ src/Dice.js
@@ -6,11 +6,15 @@ class Dice extends Component {
   render() {
     return <div className="Dice">
       {this.props.dice.map((d, idx) =>
-        <Die handleClick={this.props.handleClick}
+        <Die 
+          handleClick={this.props.handleClick}
           val={d}
           locked={this.props.locked[idx]}
           idx={idx}
-          key={idx} />
+          key={idx} 
+          disabled={this.props.disabled}
+          rolling={this.props.rolling && !this.props.locked[idx]}
+        />
       )}
     </div>
   }
--- src/Die.js
+++ src/Die.js
@@ -3,7 +3,8 @@ import "./Die.css";
 
 class Die extends Component {
   static defaultProps = {
-    numberWords: ["one", "two", "three", "four", "five", "six"]
+    numberWords: ["one", "two", "three", "four", "five", "six"],
+    val: 6
   }
   constructor(props){
     super(props);
@@ -16,13 +17,16 @@ class Die extends Component {
   }
 
   render() {
-    let classes = `Die fas fa-dice-${this.props.numberWords[this.props.val - 1]} fa-5x `;
-    if(this.props.locked) classes += 'Die-locked';
+    const {locked, val, numberWords, disabled, rolling} = this.props;
+    let classes = `Die fas fa-dice-${numberWords[val - 1]} fa-5x `;
+    if(locked) classes += 'Die-locked ';
+    if(rolling) classes += 'Die-rolling';
     return (
       <i
         className={classes}
-        style={{ backgroundColor: this.props.locked ? "grey" : "black" }}
+        style={{ backgroundColor: locked ? "grey" : "black" }}
         onClick={this.handleClick}
+        disabled={disabled}
       >
       </i>
     );
